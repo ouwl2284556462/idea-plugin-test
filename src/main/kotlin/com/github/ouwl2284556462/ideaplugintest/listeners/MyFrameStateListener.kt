2459a6a244079d62fa7987a1e9254594f3b0dfa3
GitHub Action <action@github.com>
2023-05-15 08:04:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/ouwl2284556462/ideaplugintest/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/ouwl2284556462/ideaplugintest/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ouwl2284556462.ideaplugintest.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/ouwl2284556462/ideaplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ouwl2284556462/ideaplugintest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ouwl2284556462.ideaplugintest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ouwl2284556462.ideaplugintest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
